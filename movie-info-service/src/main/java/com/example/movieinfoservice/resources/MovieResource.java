98352da52af20785fce61833d7395099a6eb14c5
Mo'men Gharib <momengharib22@hotmail.com>
2024-03-12 00:08:43 +0200

    last commit reverted

--- movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
+++ movie-info-service/src/main/java/com/example/movieinfoservice/resources/MovieResource.java
@@ -2,13 +2,20 @@ package com.example.movieinfoservice.resources;
 
 import com.example.movieinfoservice.models.Movie;
 import com.example.movieinfoservice.models.MovieSummary;
-import com.example.movieinfoservice.repository.MovieInfoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import static com.mongodb.client.model.Filters.eq;
 
 @RestController
 @RequestMapping("/movies")
@@ -18,24 +25,37 @@ public class MovieResource {
     private String apiKey;
 
     private final RestTemplate restTemplate;
-    private final MovieInfoRepository movieInfoRepository;
 
-    @Autowired
-    public MovieResource(RestTemplate restTemplate, MovieInfoRepository movieInfoRepository) {
+    public MovieResource(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.movieInfoRepository = movieInfoRepository;
     }
 
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
-        MovieSummary movieSummary = movieInfoRepository.findMovieSummaryById(movieId);
+        MovieSummary movieSummary = new MovieSummary();
 
-        if (movieSummary == null) {
+        MongoClientURI mongoClientURI = new MongoClientURI("mongodb://localhost:27017");
+        MongoClient mongoClient = new MongoClient(mongoClientURI);
+        MongoDatabase mongoDatabase = mongoClient.getDatabase("Movies");
+        MongoCollection<Document> mongoCollection = mongoDatabase.getCollection("movieInfo");
+        Bson projectionFields = Projections.fields(
+                Projections.include("_id", "title", "overview"),
+                Projections.excludeId());
+        Document cachedMovieDocument = mongoCollection.find(eq("_id", movieId)).projection(projectionFields).first();
+
+        boolean ok = (cachedMovieDocument != null);
+        if (ok) {
+            movieSummary.setTitle(cachedMovieDocument.getString("title"));
+            movieSummary.setOverview(cachedMovieDocument.getString("overview"));
+        } else {
             final String url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey;
             movieSummary = restTemplate.getForObject(url, MovieSummary.class);
-            movieInfoRepository.saveMovieSummary(movieSummary, movieId);
+            Document newMovieDocument = new Document();
+            newMovieDocument.append("_id", movieId);
+            newMovieDocument.append("title", movieSummary.getTitle());
+            newMovieDocument.append("overview", movieSummary.getOverview());
+            mongoCollection.insertOne(newMovieDocument);
         }
-
         return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
     }
 }
